c521205301e46bb18b10bdcab8f9d270d6fe619e
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-09-09 10:15:26 +0300

    edit table and input

---
+++ public/precache-manifest.2b5e8346049123830ca82dd0434fa9a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72d2821654606bd714a0c41ee4936b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6b5d0900754f48c21b",
+    "url": "/static/css/main.c3727bd0.chunk.css"
+  },
+  {
+    "revision": "e148b7ef1a9c13415ee5",
+    "url": "/static/js/2.a6567d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6567d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6b5d0900754f48c21b",
+    "url": "/static/js/main.4e4c67da.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
