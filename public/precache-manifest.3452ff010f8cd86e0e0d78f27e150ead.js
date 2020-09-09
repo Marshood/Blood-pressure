cc0673d997a7e551136b6ad872323aa71a035463
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-09-09 13:11:43 +0300

    adding build files

---
+++ public/precache-manifest.3452ff010f8cd86e0e0d78f27e150ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf8692debb8c661bf918e2b6cbba528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b526c885bc27735df42",
+    "url": "/static/css/main.adc419ce.chunk.css"
+  },
+  {
+    "revision": "e148b7ef1a9c13415ee5",
+    "url": "/static/js/2.a6567d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6567d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b526c885bc27735df42",
+    "url": "/static/js/main.ff4522ae.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
